7593bc2d566576e72adcf7054fa7d13fe70bea60
Dylan Randle <dylanrandle@rogers.com>
2019-11-07 16:58:06 -0500

    update slurm job

--- denn/sho/gan_sho_hyperslurm.sh
+++ denn/sho/gan_sho_hyperslurm.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -J gan_sho_hyper
 #SBATCH -p shared
-#SBATCH -n 256
-#SBATCH --mem 100000 # Memory request (100Gb)
-#SBATCH -t 0-12:00 # Maximum execution time (D-HH:MM)
+#SBATCH -n 32
+#SBATCH --mem 12000 # Memory request (12Gb)
+#SBATCH -t 0-24:00 # Maximum execution time (D-HH:MM)
 #SBATCH -o logs.out # Standard output
 #SBATCH -e logs.err # Standard error
 module load Anaconda3/5.0.1-fasrc01
