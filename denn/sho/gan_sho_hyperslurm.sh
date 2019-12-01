04cc229465fda5986bf054c677702596503e5d9d
Dylan Randle <dylanrandle@rogers.com>
2019-12-01 10:52:14 -0500

    new hypertune job for unsupervised gan

--- denn/sho/gan_sho_hyperslurm.sh
+++ denn/sho/gan_sho_hyperslurm.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -J gan_sho_hyper
-#SBATCH -p shared
-#SBATCH -n 32
-#SBATCH --mem 12000 # Memory request (12Gb)
-#SBATCH -t 0-24:00 # Maximum execution time (D-HH:MM)
+#SBATCH -p test
+#SBATCH -n 96
+#SBATCH --mem 34000 # Memory request (12Gb)
+#SBATCH -t 0-08:00 # Maximum execution time (D-HH:MM)
 #SBATCH -o logs.out # Standard output
 #SBATCH -e logs.err # Standard error
 module load Anaconda3/5.0.1-fasrc01
