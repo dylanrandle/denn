62b3471d4d410525b11bf7d5a34244522fcdf1b5
Dylan Randle <dylanrandle@rogers.com>
2019-12-17 09:40:40 -0500

    add L2 for nlo hypertune

--- denn/config.py
+++ denn/config.py
@@ -1,12 +1,14 @@
 import torch.nn as nn
+import numpy as np
 
 # ==========================
 # Setup of the problem
 # ==========================
 
 problem_kwargs = dict(
-    n=400,
-    perturb=False
+    n=1000,
+    perturb=True,
+    t_max=8*np.pi
 )
 
 # ==========================
@@ -30,7 +32,7 @@ gan_kwargs = dict(
     wgan=True,
     gp=0.1,
     conditional=True,
-    plot=False,
+    plot=True,
     save=True,
     fname='train_GAN.png',
 )
@@ -52,7 +54,7 @@ gen_kwargs = dict(
 disc_kwargs = dict(
     in_dim=2,
     out_dim=1,
-    n_hidden_units=32,
+    n_hidden_units=16,
     n_hidden_layers=3,
     activation=nn.Tanh(),
     residual=True,
@@ -68,7 +70,7 @@ L2_kwargs = dict(
     method='unsupervised',
     niters=10000,
     lr=1e-3,
-    betas=(0, 0.9),
+    betas=(0., 0.9),
     lr_schedule=True,
     obs_every=1,
     d1=1,
@@ -82,8 +84,31 @@ L2_mlp_kwargs = dict(
     in_dim=1,
     out_dim=1,
     n_hidden_units=32,
-    n_hidden_layers=3,
+    n_hidden_layers=4,
     activation=nn.Tanh(),
     residual=True,
     regress=True,
+)
+
+# ==========================
+# Hyper tuning
+# ==========================
+
+# GAN
+# hyper_space = dict(
+#     # disc_kwargs
+#     disc_n_hidden_units = [32],
+#     disc_n_hidden_layers = [2, 3, 4, 5],
+#     # gen_kwargs
+#     gen_n_hidden_units = [32],
+#     gen_n_hidden_layers = [2, 3, 4, 5],
+# )
+
+# L2
+hyper_space = dict(
+    # model_kwargs
+    model_n_hidden_units=[32, 64],
+    model_n_hidden_layers=[4, 6, 8],
+    # train_kwargs
+    train_niters=[10000, 50000, 100000],
 )
--- denn/nlo/hypertune.py
+++ denn/nlo/hypertune.py
@@ -4,7 +4,7 @@ import numpy as np
 import argparse
 
 from denn.problems import NonlinearOscillator
-from denn.experiments import gan_experiment
+from denn.experiments import gan_experiment, L2_experiment
 import denn.config as cfg
 from denn.utils import handle_overwrite, dict_product
 
@@ -36,9 +36,35 @@ def gan_exp_with_hypers(hypers):
     print(f'Result: {res}')
     return res
 
+def L2_exp_with_hypers(hypers):
+    """
+    take hypers dict and map values onto appropriate kwargs dict
+    to run gan_experiment
+    """
+    model_kwargs = cfg.L2_mlp_kwargs
+    train_kwargs = cfg.L2_kwargs
+
+    for k, v in hypers.items():
+        if k.startswith('model_'):
+            model_kwargs[k.replace('model_', '')] = v
+        elif k.startswith('train_'):
+            train_kwargs[k.replace('train_', '')] = v
+
+    exp_res = L2_experiment(
+        problem = NonlinearOscillator(**cfg.problem_kwargs),
+        seed = 0,
+        model_kwargs = model_kwargs,
+        train_kwargs = train_kwargs,
+    )
+    res = {'mse': exp_res['final_mse'], 'hypers': hypers}
+    print(f'Result: {res}')
+    return res
+
 
 if __name__== "__main__":
     args = argparse.ArgumentParser()
+    args.add_argument('--gan', action='store_true', default=False,
+        help='whether to use GAN-based training, default False (use L2-based)')
     args.add_argument('--ncpu', type=int, default=4,
         help='number of cpus to use (default: 4)')
     args.add_argument('--fname', type=str, default='NLO_hypertune.csv',
@@ -47,20 +73,14 @@ if __name__== "__main__":
 
     handle_overwrite(args.fname)
 
-    # define search space
-    hyper_space = dict(
-        # disc_kwargs
-        disc_n_hidden_units = [64, 32, 16],
-        disc_n_hidden_layers = [2, 3, 4],
-        # gen_kwargs
-        gen_n_hidden_units = [32, 16],
-        gen_n_hidden_layers = [3],
-    )
-
+    hyper_space = cfg.hyper_space
     hyper_space = dict_product(hyper_space)
 
     pool = mp.Pool(args.ncpu)
-    results = pool.map(gan_exp_with_hypers, hyper_space)
+    if args.gan:
+        results = pool.map(gan_exp_with_hypers, hyper_space)
+    else:
+        results = pool.map(L2_exp_with_hypers, hyper_space)
 
     pd.DataFrame().from_records(results).to_csv(args.fname)
     print(f'Saved results to {args.fname}')
