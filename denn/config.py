1e59dee53b83a262d7b575c46abdb1175feb485c
Dylan Randle <drandle@boslogin04.rc.fas.harvard.edu>
2019-12-19 11:41:03 -0500

    add updated config from experiments

--- denn/config.py
+++ denn/config.py
@@ -5,6 +5,12 @@ import numpy as np
 # Setup of the problem
 # ==========================
 
+## SHO PARAMS
+## n=100 | perturb = True | t_max = 4 * np.pi
+
+## NLO PARAMS
+## n=1000 | perturb = True | t_max = 8 * np.pi
+
 problem_kwargs = dict(
     n=1000,
     perturb=True,
@@ -14,6 +20,9 @@ problem_kwargs = dict(
 # ==========================
 # GAN
 # ==========================
+
+## SHO PARAMS
+## niters: 10K | Gen: 16x3 | Disc: 32x2
 
 # GAN Algorithm
 gan_kwargs = dict(
@@ -37,14 +46,11 @@ gan_kwargs = dict(
     fname='train_GAN.png',
 )
 
-## Settings for SHO
-## Gen: 16x3 | Disc: 32x2
-
 # Generator MLP
 gen_kwargs = dict(
     in_dim=1,
     out_dim=1,
-    n_hidden_units=32,
+    n_hidden_units=16,
     n_hidden_layers=3,
     activation=nn.Tanh(),
     residual=True,
@@ -54,8 +60,8 @@ gen_kwargs = dict(
 disc_kwargs = dict(
     in_dim=2,
     out_dim=1,
-    n_hidden_units=16,
-    n_hidden_layers=3,
+    n_hidden_units=32,
+    n_hidden_layers=2,
     activation=nn.Tanh(),
     residual=True,
     regress=True, # true for WGAN, false otherwise
@@ -65,10 +71,14 @@ disc_kwargs = dict(
 # L2 (Lagaris)
 # ==========================
 
+
+## NLO PARAMS
+## niters: 200K | MLP: 32x8
+
 # L2 Algorithm
 L2_kwargs = dict(
     method='unsupervised',
-    niters=10000,
+    niters=200000,
     lr=1e-3,
     betas=(0., 0.9),
     lr_schedule=True,
@@ -84,7 +94,7 @@ L2_mlp_kwargs = dict(
     in_dim=1,
     out_dim=1,
     n_hidden_units=32,
-    n_hidden_layers=4,
+    n_hidden_layers=8,
     activation=nn.Tanh(),
     residual=True,
     regress=True,
@@ -95,20 +105,22 @@ L2_mlp_kwargs = dict(
 # ==========================
 
 # GAN
-# hyper_space = dict(
-#     # disc_kwargs
-#     disc_n_hidden_units = [32],
-#     disc_n_hidden_layers = [2, 3, 4, 5],
-#     # gen_kwargs
-#     gen_n_hidden_units = [32],
-#     gen_n_hidden_layers = [2, 3, 4, 5],
-# )
+hyper_space = dict(
+     # gan_kwargs
+     gan_niters = [100000],
+     # disc_kwargs
+     disc_n_hidden_units = [32],
+     disc_n_hidden_layers = [4, 6, 8, 10],
+     # gen_kwargs
+     gen_n_hidden_units = [32],
+     gen_n_hidden_layers = [4, 6, 8, 10],
+)
 
 # L2
-hyper_space = dict(
-    # model_kwargs
-    model_n_hidden_units=[32, 64],
-    model_n_hidden_layers=[4, 6, 8],
-    # train_kwargs
-    train_niters=[10000, 50000, 100000],
-)
+# hyper_space = dict(
+#    # model_kwargs
+#    model_n_hidden_units=[32, 64],
+#    model_n_hidden_layers=[4, 6, 8],
+#    # train_kwargs
+#    train_niters=[10000, 50000, 100000],
+#)
