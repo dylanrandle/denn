8b00d7925a8206f320fb777f8624f5d58aa1ed4e
Dylan Randle <drandle@holylogin03.rc.fas.harvard.edu>
2020-01-09 18:06:21 -0500

    run replications for niters experiments; run 100k iters experiment for SHO GAN and NLO GAN

--- denn/nlo_GAN.sh
+++ denn/nlo_GAN.sh
@@ -4,9 +4,9 @@
 #SBATCH -n 32
 #SBATCH -N 1
 #SBATCH --mem 10000 # Memory request (in MB)
-#SBATCH -t 0-04:00 # Maximum execution time (D-HH:MM)
+#SBATCH -t 0-08:00 # Maximum execution time (D-HH:MM)
 #SBATCH -o logs.out # Standard output
 #SBATCH -e logs.err # Standard error
 module load Anaconda3/5.0.1-fasrc01
 source activate denn
-python experiments.py --pkey nlo --gan --fname gan_nlo_L2_64by12.png
+python experiments.py --pkey nlo --gan --fname gan_nlo_100k.png
--- denn/sho_GAN.sh
+++ denn/sho_GAN.sh
@@ -3,9 +3,9 @@
 #SBATCH -p test
 #SBATCH -n 12
 #SBATCH --mem 10000 # Memory request (in MB)
-#SBATCH -t 0-06:00 # Maximum execution time (D-HH:MM)
+#SBATCH -t 0-08:00 # Maximum execution time (D-HH:MM)
 #SBATCH -o logs.out # Standard output
 #SBATCH -e logs.err # Standard error
 module load Anaconda3/5.0.1-fasrc01
 source activate denn
-python experiments.py --gan --pkey sho --fname sho_gan_8pi.png
+python experiments.py --gan --pkey sho --fname sho_gan_100k.png
