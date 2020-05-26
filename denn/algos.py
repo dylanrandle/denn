probably correct POS implementation; most likely cause of prior issue was computation of mse and the shapes of the vectors, weight reductions, something like that

denn/algos.py
@@ -29,10 +29,10 @@ def train_GAN(G, D, problem, method='unsupervised', niters=100,
     grid = problem.get_grid()
     soln = problem.get_solution(grid)
 
-    # observer mask and masked grid/solution (t_obs/y_obs)
-    observers = torch.arange(0, len(grid), obs_every)
-    grid_obs = grid[observers, :]
-    soln_obs = soln[observers, :]
+    # # observer mask and masked grid/solution (t_obs/y_obs)
+    # observers = torch.arange(0, len(grid), obs_every)
+    # grid_obs = grid[observers, :]
+    # soln_obs = soln[observers, :]
 
     # labels
     real_label = 1
@@ -168,7 +168,7 @@ def train_GAN(G, D, problem, method='unsupervised', niters=100,
           lr_scheduler_D.step()
 
         # train MSE: grid sample vs true soln
-        grid_samp, sort_ids = torch.sort(grid_samp, axis=0)
+        # grid_samp, sort_ids = torch.sort(grid_samp, axis=0)
         pred = G(grid_samp)
         pred_adj = problem.adjust(pred, grid_samp)['pred']
         sol_samp = problem.get_solution(grid_samp)
@@ -242,9 +242,9 @@ def train_L2(model, problem, method='unsupervised', niters=100,
     grid = problem.get_grid()
     sol = problem.get_solution(grid)
 
-    observers = torch.arange(0, len(grid), obs_every)
-    grid_obs = grid[observers, :]
-    sol_obs = sol[observers, :]
+    # observers = torch.arange(0, len(grid), obs_every)
+    # grid_obs = grid[observers, :]
+    # sol_obs = sol[observers, :]
 
     # optimizers & loss functions
     opt = torch.optim.Adam(model.parameters(), lr=lr, betas=betas)
@@ -291,7 +291,7 @@ def train_L2(model, problem, method='unsupervised', niters=100,
             loss_trace.append(loss.item())
 
         # train MSE: grid sample vs true soln
-        grid_samp, sort_ids = torch.sort(grid_samp, axis=0)
+        # grid_samp, sort_ids = torch.sort(grid_samp, axis=0)
         pred = model(grid_samp)
         try:
             pred_adj = problem.adjust(pred, grid_samp)['pred']

denn/problems.py
@@ -414,12 +414,7 @@ class PoissonEquation(Problem):
         ).reshape(-1)
         # take minimum spacing, which is equal to spacing along an axis
         self.spacing = (xmax - xmin) / nx
-        # self.xgrid[1] - self.xgrid[0]
 
-        # set up our grid as just the set of all point tuples
-        # self.grid = torch.cartesian_prod(self.xgrid, self.ygrid)
-        # flip axis
-        # self.grid = torch.flip(self.grid, [1])
         grid_x, grid_y = torch.meshgrid(self.xgrid, self.ygrid)
         self.grid = torch.cat([grid_x.reshape(-1,1), grid_y.reshape(-1,1)], 1)
 
@@ -428,6 +423,8 @@ class PoissonEquation(Problem):
         #     - use Fenics (finite elements) to compute solution
         #     - TODO: calculate at `grid` instead of the current fixed mesh
         # """
+        # set up our grid as just the set of all point tuples
+        # self.grid = torch.cartesian_prod(self.xgrid, self.ygrid)
         # to match Fenics Mesh order
         # self.grid = torch.flip(self.grid, [1])
         # # u_np, mesh_np = poisson_compute_solution(self.nx, self.ny, self.f)
@@ -437,15 +434,12 @@ class PoissonEquation(Problem):
         return self.grid
 
     def get_grid_sample(self):
-        # return self.sample_grid(self.grid, self.spacing)
-        tmp = self.sample_grid(self.grid, self.spacing)
-        mask = np.random.choice(len(tmp), size=self.batch_size, replace=False)
-        return tmp[mask,:]
+        return self.sample_grid(self.grid, self.spacing)
 
     def get_solution(self, grid):
-        x, y = grid[:,0], grid[:, 1]
+        x, y = grid[:, 0].reshape(-1,1), grid[:, 1].reshape(-1,1)
         sol = torch.sin(np.pi*y) * torch.sinh(np.pi*(1 - x)) / torch.sinh(self.pi)
-        return sol.reshape(-1, 1)
+        return sol.flatten() # sol.reshape(-1, 1)
 
     def _poisson_eqn(self, d2x, d2y):
         """ return LHS of equation (should equal 0) """
@@ -463,16 +457,14 @@ class PoissonEquation(Problem):
         thanks to Feiyu Chen for this:
         https://github.com/odegym/neurodiffeq/blob/master/neurodiffeq/pde.py
         """
-        # def _bc(yb):
-            # tmp = torch.sin(np.pi * yb)
-
-        self.x_min_val = lambda yb: torch.sin(np.pi * yb) # _bc(yb)
+        self.x_min_val = lambda yb: torch.sin(np.pi * yb)
         self.x_max_val = lambda yb: 0
         self.y_min_val = lambda xb: 0
         self.y_max_val = lambda xb: 0
 
-        # u = u.flatten()
-        x, y = grid[:, 0], grid[:, 1]
+        u = u.reshape(-1, 1)
+
+        x, y = grid[:, 0].reshape(-1, 1), grid[:, 1].reshape(-1, 1)
 
         x_tilde = (x-self.xmin) / (self.xmax-self.xmin)
         y_tilde = (y-self.ymin) / (self.ymax-self.ymin)
@@ -483,22 +475,19 @@ class PoissonEquation(Problem):
                  y_tilde *( self.y_max_val(x) - ((1-x_tilde)*self.y_max_val(self.xmin * torch.ones_like(x_tilde))
                                                   + x_tilde *self.y_max_val(self.xmax * torch.ones_like(x_tilde))) )
 
-        u_adj = Axy + x_tilde*(1-x_tilde)*y_tilde*(1-y_tilde)*u.flatten()
+        u_adj = Axy + x_tilde*(1-x_tilde)*y_tilde*(1-y_tilde) * u
 
-        # first deriv
-        dx = diff(u_adj, x)
-        dy = diff(u_adj, y)
+        d2x = diff(u_adj, x, order=2)
+        d2y = diff(u_adj, y, order=2)
 
-        # second deriv
-        d2x = diff(dx, x)
-        d2y = diff(dy, y)
-
-        return {'pred': u_adj.reshape(-1,1), 'd2x': d2x.reshape(-1,1), 'd2y': d2y.reshape(-1,1)}
+        return {'pred': u_adj, 'd2x': d2x, 'd2y': d2y}
 
     def get_plot_dicts(self, pred, grid, sol):
         """ return appropriate pred_dict / diff_dict used for plotting """
         adj = self.adjust(pred, grid)
         pred_adj, d2x, d2y = adj['pred'], adj['d2x'], adj['d2y']
+        # abs_diff = np.abs((pred_adj[:,0] - sol[:,0]).detach())
+        # pred_dict = {"abs_diff": abs_diff}
         pred_dict = {'$\hat{u}$': pred_adj.detach()}
 
         resid = self.get_equation(pred, grid)

denn/utils.py
@@ -8,16 +8,40 @@ from IPython.display import clear_output
 import pandas as pd
 
 # global plot params
-plt.rc('axes', titlesize=15)
-plt.rc('axes', labelsize=12)
-plt.rc('legend', fontsize=12)
+# plt.rc('axes', titlesize=15)
+# plt.rc('axes', labelsize=12)
+# plt.rc('legend', fontsize=12)
 
-def diff(x, t):
-    """ wraps autograd to perform differentiation """
-    dx_dt, = autograd.grad(x, t,
-                           grad_outputs=x.data.new(x.shape).fill_(1),
-                           create_graph=True)
-    return dx_dt
+plt.rc('axes', titlesize=20)
+plt.rc('axes', labelsize=20)
+plt.rc('legend', fontsize=20)
+plt.rc('xtick', labelsize=15)
+plt.rc('ytick', labelsize=15)
+
+# def diff(x, t):
+#     """ wraps autograd to perform differentiation """
+#     dx_dt, = autograd.grad(x, t,
+#                            grad_outputs=x.data.new(x.shape).fill_(1),
+#                            create_graph=True)
+#     return dx_dt
+
+def diff(x, t, order=1):
+    """The derivative of a variable with respect to another.
+    :param x: The :math:`x` in :math:`\\displaystyle\\frac{\\partial x}{\\partial t}`.
+    :type x: `torch.tensor`
+    :param t: The :math:`t` in :math:`\\displaystyle\\frac{\\partial x}{\\partial t}`.
+    :type t: `torch.tensor`
+    :param order: The order of the derivative, defaults to 1.
+    :type order: int
+    :returns: The derivative.
+    :rtype: `torch.tensor`
+    """
+    ones = torch.ones_like(x)
+    der, = autograd.grad(x, t, create_graph=True, grad_outputs=ones)
+    for i in range(1, order):
+        ones = torch.ones_like(der)
+        der, = autograd.grad(der, t, create_graph=True, grad_outputs=ones)
+    return der
 
 def plot_results(mse_dict, loss_dict, grid, pred_dict, diff_dict=None, clear=False,
     save=False, dirname=None, logloss=False, alpha=0.8):
@@ -66,11 +90,12 @@ def plot_results(mse_dict, loss_dict, grid, pred_dict, diff_dict=None, clear=Fal
 
     # Predictions
     if grid.shape[1] == 2: # PDE
-        x = np.unique(grid[:, 0])
-        y = np.unique(grid[:, 1])
+        x, y = grid[:, 0], grid[:, 1]
+        xdim, ydim = int(np.sqrt(len(x))), int(np.sqrt(len(y)))
+        xx, yy = x.reshape((xdim, ydim)), y.reshape((xdim, ydim))
         for i, (k, v) in enumerate(pred_dict.items()):
-            v = v.reshape((len(x),len(y)))
-            cf = ax[2].contourf(x, y, v.T, cmap='Reds')
+            v = v.reshape((xdim, ydim))
+            cf = ax[2].contourf(xx, yy, v, cmap='Reds')
             cb = fig.colorbar(cf, format='%.0e', ax=ax[2])
         ax[2].set_xlabel('$x$')
         ax[2].set_ylabel('$y$')
@@ -87,11 +112,12 @@ def plot_results(mse_dict, loss_dict, grid, pred_dict, diff_dict=None, clear=Fal
     # Derivatives
     if diff_dict:
         if grid.shape[1] == 2: # PDE
-            x = np.unique(grid[:, 0])
-            y = np.unique(grid[:, 1])
+            x, y = grid[:, 0], grid[:, 1]
+            xdim, ydim = int(np.sqrt(len(x))), int(np.sqrt(len(y)))
+            xx, yy = x.reshape((xdim, ydim)), y.reshape((xdim, ydim))
             for i, (k, v) in enumerate(diff_dict.items()):
-                v = v.reshape((len(x),len(y)))
-                cf = ax[3].contourf(x, y, v.T, cmap='Reds')
+                v = v.reshape((xdim, ydim))
+                cf = ax[3].contourf(xx, yy, v, cmap='Reds')
                 cb = fig.colorbar(cf, format='%.0e', ax=ax[3])
             ax[3].set_xlabel('$x$')
             ax[3].set_ylabel('$y$')