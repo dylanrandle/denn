1258ed3adbe036f49981518fe0510d40b9a27a6d
blakebullwinkel <jblake1@live.com>
2021-09-18 10:03:20 -0400

    updated bash script for fasrc

--- slurm/ray_tune.sh
+++ slurm/ray_tune.sh
@@ -7,6 +7,10 @@
 #SBATCH -t 0-02:00 # Maximum execution time (D-HH:MM)
 #SBATCH -o logs.out # Standard output
 #SBATCH -e logs.err # Standard error
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-user=jbullwinkel@fas.harvard.edu
 module load Anaconda3/5.0.1-fasrc01
+conda env create -f environment_from_history.yml
 source activate denn
-python ray_tune.py --pkey sho
+cd ../denn
+python ray_tune.py --pkey sir
