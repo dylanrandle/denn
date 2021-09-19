4e0b82354d7c15fc9fb13a3b8274764629af818a
blakebullwinkel <jblake1@live.com>
2021-09-19 09:44:04 -0400

    added ray tune bash scripts for more experiments

--- slurm/ray_tune_exp.sh
+++ slurm/ray_tune_exp.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-#SBATCH -J ray_tune
-#SBATCH -p test
+#SBATCH -J ray_tune_exp
+#SBATCH -p shared
 #SBATCH -n 48
 #SBATCH -N 1
 #SBATCH --mem 96000 # Memory request (in MB)
-#SBATCH -t 0-02:00 # Maximum execution time (D-HH:MM)
+#SBATCH -t 0-01:00 # Maximum execution time (D-HH:MM)
 #SBATCH -o logs.out # Standard output
 #SBATCH -e logs.err # Standard error
-#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
 #SBATCH --mail-user=jbullwinkel@fas.harvard.edu
-module load Anaconda3/5.0.1-fasrc01
-conda env create -f environment_from_history.yml
+module load gcc/10.2.0-fasrc01
+module load Anaconda3/2020.11
 source activate denn
 cd ../denn
-python ray_tune.py --pkey sir
+python ray_tune.py --pkey exp --ncpu 48 --nsample 200
---
+++ slurm/ray_tune_pos.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -J ray_tune_pos
+#SBATCH -p shared
+#SBATCH -n 48
+#SBATCH -N 1
+#SBATCH --mem 96000 # Memory request (in MB)
+#SBATCH -t 0-02:00 # Maximum execution time (D-HH:MM)
+#SBATCH -o logs.out # Standard output
+#SBATCH -e logs.err # Standard error
+#SBATCH --mail-type=END
+#SBATCH --mail-user=jbullwinkel@fas.harvard.edu
+module load gcc/10.2.0-fasrc01
+module load Anaconda3/2020.11
+source activate denn
+cd ../denn
+python ray_tune.py --pkey pos --ncpu 48 --nsample 200
