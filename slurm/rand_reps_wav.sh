64a32ca59246ef4daeb645473f0000a72ee757fa
blakebullwinkel <jblake1@live.com>
2021-10-26 15:44:28 -0400

    added slurm files for WAV problem

---
+++ slurm/rand_reps_wav.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -J rand_reps_wav
+#SBATCH -p test
+#SBATCH -n 48
+#SBATCH -N 1
+#SBATCH --mem 96000 # Memory request (in MB)
+#SBATCH -t 0-04:00 # Maximum execution time (D-HH:MM)
+#SBATCH -o logs.out # Standard output
+#SBATCH -e logs.err # Standard error
+#SBATCH --mail-type=END
+#SBATCH --mail-user=jbullwinkel@fas.harvard.edu
+module load gcc/10.2.0-fasrc01
+module load Anaconda3/2020.11
+source activate denn
+cd ../denn
+python rand_reps.py --pkey wav --gan --nreps 20 --fname rand_reps_wav
