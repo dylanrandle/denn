88b5951309f96ecf72dddf24a0e153636018dbd5
blakebullwinkel <jblake1@live.com>
2021-11-09 00:32:37 -0500

    added batch scripts for BUR

---
+++ slurm/ray_tune_bur.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -J ray_tune_bur
+#SBATCH -p test
+#SBATCH -n 48
+#SBATCH -N 1
+#SBATCH --mem 96000 # Memory request (in MB)
+#SBATCH -t 0-08:00 # Maximum execution time (D-HH:MM)
+#SBATCH -o logs.out # Standard output
+#SBATCH -e logs.err # Standard error
+#SBATCH --mail-type=END
+#SBATCH --mail-user=jbullwinkel@fas.harvard.edu
+module load gcc/10.2.0-fasrc01
+module load Anaconda3/2020.11
+source activate denn
+cd ../denn
+python ray_tune.py --pkey bur --ncpu 48 --nsample 2000
