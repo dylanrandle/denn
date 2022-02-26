a6fe9e2ae780c5a54a7127acd8374c38e110485e
blakebullwinkel <jblake1@live.com>
2022-02-26 18:14:29 -0500

    updated slurm files

--- slurm/rays_GAN.sh
+++ slurm/rays_GAN.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH -J rand_reps_exp
+#SBATCH -J rays_gan
 #SBATCH -p test
 #SBATCH -n 48
 #SBATCH -N 1
 #SBATCH --mem 96000 # Memory request (in MB)
-#SBATCH -t 0-02:00 # Maximum execution time (D-HH:MM)
+#SBATCH -t 0-08:00 # Maximum execution time (D-HH:MM)
 #SBATCH -o logs.out # Standard output
 #SBATCH -e logs.err # Standard error
 #SBATCH --mail-type=END
@@ -13,4 +13,4 @@ module load gcc/10.2.0-fasrc01
 module load Anaconda3/2020.11
 source activate denn
 cd ../denn
-python rand_reps.py --pkey exp --gan --nreps 10 --fname rand_reps_exp
+echo "y" | python experiments.py --gan --pkey rays
